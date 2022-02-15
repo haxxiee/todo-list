298a388c7f0f5be21e0b25f30a955cf8774ad09d
Linus Foxell <linusfoxell@hotmail.com>
2022-02-15 15:38:21 +0100

    tailwind added + map/code cleanup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="flex justify-center content-center mt-5 text-3xl font-bold">
+        Hello world
+      </h1>
+    </div>
+  );
+};
+
+export default App;
